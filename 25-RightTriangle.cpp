50ca5a0f0439f0f70a3a7ee6269ba0c52c041afe
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2024-01-03 21:31:31 +0530

    Create 25-RightTriangle.cpp

---
+++ 25-RightTriangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+ 
+    // looping rows
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j >= i) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
