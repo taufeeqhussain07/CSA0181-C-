41ebce625f1ce0b0b49f17f22d8d64816e874df3
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2024-01-03 20:52:41 +0530

    Create 17-SquarePattern.cpp

---
+++ 17-SquarePattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n,i,j;
+    cout <<"enter n::";
+    cin >> n;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
