b35c3e1cc184b3ce5f253a4e60a825afcba85774
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2024-01-03 20:26:38 +0530

    Create 08-1to10.cpp

---
+++ 08-1to10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+   cout << "numbers from 1 to 10 :: ";
+    for (int i = 1; i <= 10; ++i) {
+        cout << i << " ";
+    }
+    return 0;
+}
