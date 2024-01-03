1c4a010925e12aed368a5ce11fbda79d561fcb8c
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2024-01-03 20:18:06 +0530

    Create 04-FloatingPoint.cpp

---
+++ 04-FloatingPoint.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+double n;
+cout << "enter floating number::";
+cin >> n;
+  cout << "round up " << n << " is::" << ceil(n) << " , Round down" << n << " is::" << floor(n);
+  return 0;
+}
